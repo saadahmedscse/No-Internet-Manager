3428b088248ab2b5f5b00effdfa31e4c82096a56
saada <saadahmedscse@gmail.com>
2023-02-25 17:57:04 +0600

    tested network util library

--- app/src/main/java/com/saadahmedsoft/nointernetmanager/MainActivity.java
+++ app/src/main/java/com/saadahmedsoft/nointernetmanager/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.saadahmedev.networkutil.NetworkUtil;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NetworkUtil networkUtil = NetworkUtil.getInstance(this);
+        if (networkUtil.isInternetAvailable(NetworkUtil.SHOW_TOAST_MESSAGE)) {
+            //has
+        }
     }
 }
